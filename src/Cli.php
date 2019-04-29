7a1067aa0e8392183ab6f01c62c8d1905ad1e7de
Pavel <mikhailov.sztu@gmail.com>
2019-04-29 19:29:36 +0300

    init project

---
+++ src/Cli.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Gendiff\Cli;
+
+use Docopt;
+
+function run()
+{
+    $doc = <<<DOC
+    Generate diff
+
+    Usage:
+      gendiff (-h|--help)
+      gendiff [--format <fmt>] <firstFile> <secondFile>
+
+    Options:
+      -h --help                     Show this screen
+      --format <fmt>                Report format [default: pretty]
+
+    DOC;
+
+    Docopt::handle($doc);
+}
